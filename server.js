89adec81b2f8944c0e6dfbe534bf0c669bdea208
Diks7106 <dikshantsavaliya2000@gmail.com>
2023-08-23 17:24:43 +0530

    check health

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+app.get("/check-health",(req,res)=>{
+    res.send({message:"Success"})
+})
+
 app.use("/Attendance", attendance);
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 
